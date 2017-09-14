6da489ff441ee06d28a8ec8bf4e3a2f3d1cc5615
scandar <thescandar@gmail.com>
2017-09-14 14:51:03 +0200

    express

--- testing-express/app.js
+++ testing-express/app.js
@@ -1,8 +1,40 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
+app.set('view engine', 'ejs');
+// middleware for static files
+app.use('/assets', express.static('assets'));
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
 app.get('/',function(req,res) {
-    res.send('test');
+    res.render('index');
+});
+
+app.get('/profile/:name', function (req,res) {
+    var data = {
+        name: req.params.name,
+        age: 20,
+        hobbies: ['coding', 'music', 'movies']
+    };
+
+    res.render('profile', {data: data});
+});
+
+app.get('/contact', function (req,res) {
+    res.render('contact', {qs:req.query});
+});
+
+app.post('/contact', urlencodedParser,function (req, res) {
+    // if(!req.body) return res.send(400);
+    res.render('contact-success', {data: req.body});
+});
+
+app.get('*', function (req,res) {
+    res.render('404');
 });
 
 app.listen(3000, function (err) {
